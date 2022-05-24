c2df7774a31dcb8043aaf37dc4ffe838ccdb88cf
sschreyer <samschreyer360@gmail.com>
2022-05-24 16:56:12 +1000

    Implement clickable buttons on front page

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+let whoAmI = document.getElementById("whoAmI");
+
+whoAmI.onclick = () => {
+    console.log("hi");
+    window.location.href="/about.html";
+};
+
+
+let whatHaveIDone = document.getElementById("whatHaveIDone");
+
+whatHaveIDone.onclick = () => {
+    console.log("hi");
+    window.location.href="/projects.html";
+};
+
+
+let whatDoInFuture = document.getElementById("whatDoInFuture");
+
+whatDoInFuture.onclick = () => {
+    console.log("hi");
+    window.location.href="/future.html";
+};
